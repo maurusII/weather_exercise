06cd2ac3f1577553afdd7825374aa50934d9656e
maurusII <maurovergnani03@gmail.com>
2024-04-14 07:51:12 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const apiKey = "8011cc0e8f3bb18806c881a1ae9b2093"; //metti qui la tua chiave
+const citySelect = document.getElementById("city-select");
+const weatherCards = document.getElementById("weather-cards"); //div to fill with weather cards
+const form = document.getElementById("city-form"); //form to get the city
+// const cities = ["Milano", "Roma", "Bologna", "Palermo", "Napoli", "Torino", "Firenze"];
+let cities = document.getElementById('city-select').querySelectorAll('option');
+//for the home exercise using Promise.all
+
+function clearWeatherCards() {
+  weatherCards.innerHTML = '';
+  //removes the weather cards from the div
+}
+
+function displayWeather(data){
+  const city = data.name;
+  const temp = data.main.temp;
+  const descr = data.weather[0].description;
+  const icon = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png';
+  return `<div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${city}</h5>
+              <img class="card-img-top" src=${icon} alt="Title">
+              <p class="card-text">${temp}</p>
+              <p class="card-text">${descr}</p>
+            </div>
+          </div>`;
+}
+
+function getWeather(selectedCity, apiKey) {
+  clearWeatherCards()
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&lang=it&units=metric&appid=${apiKey}`;
+  console.log(url);
+  //completa la funzione
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      weatherCards.innerHTML = displayWeather(data);
+    })
+    .catch(error => console.error(error)); 
+}
+
+//oppure: 
+// async function getWeather(selectedCity, apiKey) {
+//   const url = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&lang=it&units=metric&appid=${apiKey}`;
+//   riempi se vuoi usare invece async await
+//}
+
+form.addEventListener("submit", event => {
+  event.preventDefault(); // prevents form from refreshing page
+  const selectedCity = document.getElementById("city-select").value;
+  getWeather(selectedCity, apiKey);
+});
+
